6f035daaa8ff636685ad62dc0e561c70b64e4f93
noothanks <coreyfuller727@yahoo.com>
2022-07-06 14:28:54 -0400

    adding post route

--- server.js
+++ server.js
@@ -1,4 +1,6 @@
 const express = require('express');
+const fs = require('fs');
+const path = require('path');
 
 //requiring animal JSON data
 const {animals} = require('./data/animals.json');
@@ -6,6 +8,13 @@ const {animals} = require('./data/animals.json');
 const PORT = process.env.PORT || 3001;
 //instantiate server
 const app = express();
+
+//middleware
+//parse incoming string or array data
+app.use(express.urlencoded({extend: true}));
+//parse incoming JSON data
+app.use(express.json());
+
 //make server listen for connections at port (destination) 3001 on the host (address)
 app.listen(PORT, () => {
     console.log(`API server now on port ${PORT}!`);
@@ -58,11 +67,53 @@ function findById(id, animalsArray) {
     return result;
 }
 
+//creating functionality for passing req.body to the animals array
+function createNewAnimal(body, animalsArray) {
+    //define animal as body received from POST request
+    const animal = body;
+    //main function code
+    //updating animals array to include added animal
+    animalsArray.push(animal);
+    //updating actual animals.json file to include new animal
+    fs.writeFileSync(
+        //join file paths from current file to the file sought after
+        //the end result is the file being written
+        path.join(__dirname, './data/animals.json'),
+        //this is the data being passed into the file
+        //null indicates we don't want to change existing data
+        //2 indicates whitespace between data
+        JSON.stringify({animals: animalsArray}, null, 2)
+    );
+    //return animal to post route for response
+    return animal;
+}
+
+//checking that the newly created animal is formatted properly
+function validateAnimal(animal) {
+    //if no animal name or not a string return false
+    if(!animal.name || typeof animal.name !== 'string') {
+        return false;
+    }
+    //if no species or not a string return false
+    if(!animal.species || typeof animal.species !== 'string') {
+        return false;
+    }
+    //if no diet or not a string return false
+    if(!animal.diet || typeof animal.diet !== 'string') {
+        return false;
+    }
+    //if no animal personality trais or if not an array return false
+    if(!animal.personalityTraits || !Array.isArray(animal.personalityTraits)) {
+        return false;
+    }
+    return true;
+}
 //adding route to access animals.json data
 //get() method looks for string describing client fetch route
 //get() methods looks for callback to execute on request
 //callback uses corresponding method to say 'hello' for res (response)
 //or json() for JSON animals data
+//method indicates which type of data is being received to ensure it is delivered properly
 app.get('/api/animals', (req, res) => {
     //define data sought after
     let results = animals;
@@ -88,4 +139,23 @@ app.get('/api/animals/:id', (req, res) => {
     } else {
         res.send(404);
     }
-})
+});
+
+//setting up client request for the server to accept data
+app.post('/api/animals', (req, res) => {
+    //set new animal id to the next index of the array based on array length
+    //converts id to string and saves new animal id to the body
+    req.body.id = animals.length.toString();
+
+    //add animal to json file and animals array
+    //check if any data in req.body is incorrect
+    //if so send back 400 error
+     if(!validateAnimal(req.body)){ 
+        res.status(400).send('The animal is not formatted correctly.');
+     } else {
+        //define data sought after
+        const animal = createNewAnimal(req.body, animals);
+        //req.body is where incoming content will be populated
+        res.json(req.body);
+    }
+});
